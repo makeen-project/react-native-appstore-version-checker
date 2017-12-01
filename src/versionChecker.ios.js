4246773eddb313fe19626f546922efc57ff37844
Catalin Rizea <catalinrizea1@gmail.com>
2017-12-01 11:00:59 +0200

    adding support for entity parameter to query for tvSoftware

--- src/versionChecker.ios.js
+++ src/versionChecker.ios.js
@@ -1,8 +1,13 @@
 import result from 'lodash/result';
 import {get, parseJson} from './fetcher';
 
-const getAppstoreAppVersion = (identifier, { typeOfId = 'id', country = 'us' }) => {
-  const url = `http://itunes.apple.com/lookup?${typeOfId}=${identifier}&country=${country}`;
+const getAppstoreAppVersion = (identifier, { typeOfId = 'id', country = 'us', entity = null }) => {
+  let url = `http://itunes.apple.com/lookup?${typeOfId}=${identifier}&country=${country}`;
+
+  if (entity) {
+    url += `&entity=${entity}`;
+  }
+
   return get(url).then(parseJson).then((d) => {
     const version = result(d, 'data.results[0].version');
     if (!version) {
